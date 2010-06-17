d62f19e27005b8792b8dadf74e69c12be2fd7f97
Troels Henriksen <athas@sigkill.dk>
2010-06-17 23:15:19 +0200

    Bump version number to 2.1-dev.

--- Main.hs
+++ Main.hs
@@ -146,7 +146,7 @@ usageStr = do
   return $ usageInfo header options
 
 versionStr :: String
-versionStr = "2.0"
+versionStr = "2.1-dev"
 
 options :: [GSMenuOption a]
 options = [ Option "h" (NoArg $ \_ -> do
